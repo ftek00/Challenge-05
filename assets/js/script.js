848196d8c63d92bdaf3bc35452228ed445ec1334
Feleg Teklezghi <felegteklezghi@gmail.com>
2024-07-01 18:43:09 -0400

    created func to gen unique task id

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,10 @@ let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
-function generateTaskId() {}
+function generateTaskId() {
+  nextId++;
+  return nextId;
+}
 // Todo: create a function to create a task card
 function createTaskCard(task) {}
 // Todo: create a function to render the task list and make cards draggable
